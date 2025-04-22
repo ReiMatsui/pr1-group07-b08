ff24404236df0c83a52d26c1bc3bc06ebcc9227c
Fukuoka-hub <huifhahe38128@gmail.com>
2025-04-22 17:52:26 +0900

    エラー修正

--- chari-spot/frontend/lib/main.dart
+++ chari-spot/frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,7 +41,8 @@ class _ParkingMapScreenState extends State<ParkingMapScreen> {
             children: [
               TileLayer(
                 urlTemplate:
-                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                tileProvider: CancellableNetworkTileProvider(),
                 subdomains: ['a', 'b', 'c'],
               ),
               MarkerLayer(
