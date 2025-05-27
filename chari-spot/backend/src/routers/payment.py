3ea915ea9b79e9da47f7e6bd10ee0528413d1364
Yiming <zhou_yiming0109@outlook.com>
2025-05-27 17:13:18 +0900

    refactor

--- chari-spot/backend/src/routers/payment.py
+++ chari-spot/backend/src/routers/payment.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -12,48 +13,29 @@ router = APIRouter(
     dependencies=[Depends(get_current_user)]
 )
 
-# create a new payment
+
 @router.post("/park", response_model=schemas.PaymentResponse)
-def park(
+def update(
     spot_id: int = Query(..., description="Spot ID"),
     slot_id: int = Query(..., description="Slot ID"),
+    parked: Optional[bool] = Query(None, description="Parking status"),
+    paid: Optional[bool] = Query(None, description="Payment status"),
     db: Session = Depends(get_db),
 ):
-    paymentCreate = payment.Payment(
-        spot_id=spot_id,
-        slot_id=slot_id,
-        parked=True,
-        paid=False
-    )
     if crud.get_payment_by_spot_and_slot(db, spot_id, slot_id) is None:
+        paymentCreate = payment.Payment(
+            spot_id=spot_id,
+            slot_id=slot_id,
+            parked=True,
+            paid=False
+        )
         res = crud.create_payment(db, paymentCreate)
     else:
-        res = crud.update_payment(db, spot_id=spot_id, slot_id=slot_id, parked=True, paid=False)
+        res = crud.update_payment(db, spot_id=spot_id, slot_id=slot_id, parked=parked, paid=paid)
     
     response = schemas.PaymentResponse.model_validate(res)
     return response
 
-@router.post("/pay", response_model=schemas.PaymentResponse)
-def park_in(
-    spot_id: int = Query(..., description="Spot ID"),
-    slot_id: int = Query(..., description="Slot ID"),
-    db: Session = Depends(get_db),
-):
-    res = crud.update_payment(db, spot_id=spot_id, slot_id=slot_id, parked=True, paid=True)
-    
-    response = schemas.PaymentResponse.model_validate(res)
-    return response
-
-@router.post("/leave", response_model=schemas.PaymentResponse)
-def leave(
-    spot_id: int = Query(..., description="Spot ID"),
-    slot_id: int = Query(..., description="Slot ID"),
-    db: Session = Depends(get_db),
-):
-    res = crud.update_payment(db, spot_id=spot_id, slot_id=slot_id, parked=False, paid=False)
-    
-    response = schemas.PaymentResponse.model_validate(res)
-    return response
 
 # url/?spot_id=1&slot_id=2
 @router.get("/", response_model=None)
